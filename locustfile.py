d0b2f573741553542b94748dcb55a89763fedd1f
Edward Brunson <eddiebrunson@gmail.com>
2021-05-05 22:27:36 -0400

    Add locusfile

---
+++ locustfile.py
@@ -0,0 +1,33 @@
+import random
+from locust import HttpUser, task, between
+
+
+class QuickstartUser(HttpUser):
+    wait_time = between(2, 5)
+
+    @task
+    def index(self):
+        self.client.get("/")
+
+    @task
+    def checkPredict(self):
+        self.client.post("/predict", json={
+            "CHAS": {
+                "0": 0
+            },
+            "RM": {
+                "0": 6.575
+            },
+            "TAX": {
+                "0": 296.0
+            },
+            "PTRATIO": {
+                "0": 15.3
+            },
+            "B": {
+                "0": 396.9
+            },
+            "LSTAT": {
+                "0": 4.98
+            }
+        })
